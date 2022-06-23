849a69010cbec58150703a6814914ae7d2c9bae9
unknown <ashutoshgupta3742@gmail.com>
2022-06-23 12:41:05 +0530

    final changes

--- server/index.js
+++ server/index.js
@@ -21,6 +21,9 @@ app.use("/user", userRouter);
 
 const PORT = process.env.PORT|| 5000;
 
+
+
+
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${ PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
